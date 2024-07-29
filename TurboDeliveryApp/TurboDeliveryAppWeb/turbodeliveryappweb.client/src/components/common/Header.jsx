4c60d25a52b8a27af48b2ba9e2d50e288a2be8ff
Soel-Attala <soelattala7@gmail.com>
2024-07-29 13:54:13 -0300

    fix views, add css files, fix navbar

--- TurboDeliveryApp/TurboDeliveryAppWeb/turbodeliveryappweb.client/src/components/common/Header.jsx
+++ TurboDeliveryApp/TurboDeliveryAppWeb/turbodeliveryappweb.client/src/components/common/Header.jsx
@@ -1,20 +1,57 @@
+
+import { AppBar, Toolbar, Container } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@mui/styles';
+import './Header.css';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    appBar: {
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: theme.shadows[4],
+    },
+    logo: {
+        height: 40,
+        marginRight: theme.spacing(2),
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    menu: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        flexGrow: 1,
+    },
+    link: {
+        color: theme.palette.text.primary,
+        textDecoration: 'none',
+        margin: theme.spacing(1),
+        '&:hover': {
+            color: theme.palette.primary.main,
+        },
+    },
+}));
 
 function Header() {
+    const classes = useStyles();
+
     return (
-        <header>
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/login">Login</Link></li>
-                    <li><Link to="/register">Register</Link></li>
-                    <li><Link to="/order-form">Create Order</Link></li>
-                    <li><Link to="/order-list">Order List</Link></li>
-                    <li><Link to="/dashboard">Dashboard</Link></li>
-                    <li><Link to="/profile">Profile</Link></li>
-                </ul>
-            </nav>
-        </header>
+        <AppBar position="static" className={classes.appBar}>
+            <Toolbar>
+                <Container className={classes.menu}>
+                    <Link to="/" className="link">
+                        <img src="/path-to-your-logo.png" alt="Logo" className="logo" />
+                    </Link>
+                    <div>
+                        <Link to="/dashboard" className="link">Dashboard</Link>
+                        <Link to="/order-form" className="link">My Orders</Link>
+                        <Link to="/profile" className="link">Profile</Link>
+                    </div>
+                </Container>
+            </Toolbar>
+        </AppBar>
     );
 }
 
